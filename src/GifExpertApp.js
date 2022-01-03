5daa8c024a3fd9ebebc3f6f470b99aee93358032
Daniel Ramirez Castano <dm.ramirez@globant.com>
2022-01-03 10:12:46 -0500

    Added component AddCategory AddCategory component is able to change categorie state

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,32 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 export default function GifExpertApp() {
-    
-    // const categories = ['OnePunch', 'SamuariX', 'DragonBall']
-    const [categories, setCategories] = useState(['OnePunch', 'SamuariX', 'DragonBall'])
-    const handleAdd = () => {
 
-        // setCategories( [ ...categories, 'new Element' ] )
-        setCategories( cats => [ ...cats, 'new Element' ] )
-        
-    }
-    
-    return (
-      <div>
-        <h2> GifExpertApp </h2>
-        <hr />
+  // const categories = ['OnePunch', 'SamuariX', 'DragonBall']
+  const [categories, setCategories] = useState([ "OnePunch", "SamuariX", "DragonBall"]);
+  // const handleAdd = () => {
+  //   // setCategories( [ ...categories, 'new Element' ] )
+  //   setCategories((cats) => [...cats, "new Element"]);
+  // };
 
-        <button onClick={ handleAdd } >AddElement</button>
+  return (
+    <div>
+      <h2> GifExpertApp </h2>
+      <AddCategory setCategories={ setCategories }/>
+      <hr />
 
-        <ol>
-            { 
-                categories.map( 
-                    ( category ) => <li key={ category }>{ category }</li>
-                ) 
-            }
-        </ol>
-
-      </div>
-    )
-  }
-  
+      <ol>
+        {
+          categories.map((category, i) => (
+            <li key={ i }>{ category }</li>
+          ))
+        }
+      </ol>
+    </div>
+  );
+}
